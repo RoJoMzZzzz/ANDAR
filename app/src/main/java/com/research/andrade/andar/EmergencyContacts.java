ccc63ec03b46a65e54094b98293fbb3f396e3bf0
Jomiel Andrade <jomielandrade@gmail.com>
2016-12-10 17:54:54 +0800

    update with alarm and revisions of qr scanner

---
+++ app/src/main/java/com/research/andrade/andar/EmergencyContacts.java
@@ -0,0 +1,13 @@
+package com.research.andrade.andar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmergencyContacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_contacts);
+    }
+}
