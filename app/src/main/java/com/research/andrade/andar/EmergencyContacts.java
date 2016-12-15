e5e910b106c45af794c3d992d502a6497843b9d8
Jomiel Andrade <jomielandrade@gmail.com>
2016-12-15 22:34:20 +0800

    revise myqr and emergency contacts

--- app/src/main/java/com/research/andrade/andar/EmergencyContacts.java
+++ app/src/main/java/com/research/andrade/andar/EmergencyContacts.java
@@ -1,13 +1,46 @@
 package com.research.andrade.andar;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 public class EmergencyContacts extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_contacts);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Emergency Contacts");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton myFab = (FloatingActionButton) findViewById(R.id.fabAddEmCont);
+        myFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            EmergencyContacts.this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
