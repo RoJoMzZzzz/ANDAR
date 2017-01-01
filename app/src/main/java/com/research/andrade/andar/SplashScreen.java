2900731c1c4bfca5e7375de1051089ec568e5516
RoJoMzZzzz <jomielandrade@gmail.com>
2017-01-02 00:44:41 +0800

    added tweet feeds

--- app/src/main/java/com/research/andrade/andar/SplashScreen.java
+++ app/src/main/java/com/research/andrade/andar/SplashScreen.java
@@ -3,12 +3,22 @@ package com.research.andrade.andar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 public class SplashScreen extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "1h3yGRCRBld8xdNPenI8aDn21";
+    private static final String TWITTER_SECRET = "nE6uINZxUicaHCmhwH3hmkP5dAsjlX9rMD917L7Lgi6R7YaSs6";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_splash_screen);
 
         Thread myTh = new Thread() {
--- app/src/main/java/com/research/andrade/andar/Update.java
+++ app/src/main/java/com/research/andrade/andar/Update.java
@@ -3,9 +3,14 @@ package com.research.andrade.andar;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.twitter.sdk.android.core.*;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.tweetui.*;
 
 
 /**
@@ -23,7 +28,27 @@ public class Update extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false);
+        View v = inflater.inflate(R.layout.fragment_update, container, false);
+
+
+        // TODO: Use a more specific parent
+        final ViewGroup parentView = (ViewGroup) getActivity().getWindow().getDecorView().getRootView();
+        // TODO: Base this Tweet ID on some data from elsewhere in your app
+        long tweetId = 631879971628183552L;
+        TweetUtils.loadTweet(tweetId, new Callback<Tweet>() {
+            @Override
+            public void success(Result<Tweet> result) {
+                TweetView tweetView = new TweetView(getActivity(), result.data);
+                parentView.addView(tweetView);
+            }
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Load Tweet failure", exception);
+            }
+        });
+
+
+        return v;
     }
 
 }
