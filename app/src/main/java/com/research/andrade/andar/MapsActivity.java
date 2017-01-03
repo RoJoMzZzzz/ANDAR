ae19e0bdbe9da5aa4a4018447b151e4aa979dd4d
RoJoMzZzzz <jomielandrade@gmail.com>
2017-01-04 00:41:42 +0800

    updated MapsAactivity

--- app/src/main/java/com/research/andrade/andar/MapsActivity.java
+++ app/src/main/java/com/research/andrade/andar/MapsActivity.java
@@ -1,8 +1,22 @@
 package com.research.andrade.andar;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,9 +24,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener
+         {
 
+    private Toolbar toolbar;
     private GoogleMap mMap;
+    private double longitude, latitude;
+    private GoogleApiClient googleApiClient;
+    private EditText longiEdt, latiEdt;
+    private Button findBtn;
+
+                 //14.5641 Latitude and 121.0558 Longitude
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +46,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //Initializing googleapi client
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Tracker");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        //getFrLoc();
+
+        longiEdt = (EditText) findViewById(R.id.edtLongi);
+        latiEdt = (EditText) findViewById(R.id.edtLati);
+        findBtn = (Button) findViewById(R.id.btnFind);
+        findBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String latiInput = latiEdt.getText().toString(), longiInput = longiEdt.getText().toString();
+
+
+                if (latiInput.equals(""))
+                    latiEdt.setError("This Field can't be empty");
+                else if (longiInput.equals(""))
+                    longiEdt.setError("This Field can't be empty");
+                else {
+                    latitude = Double.parseDouble(latiInput);
+                    longitude = Double.parseDouble(longiInput);
+                    moveMap();
+                }
+            }
+        });
+
+
+
+
+
+
     }
 
 
@@ -37,10 +106,100 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng latLng = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(latLng));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        getCurrentLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    //Getting current location
+    private void getCurrentLocation() {
+        mMap.clear();
+        //Creating a location object
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if (location != null) {
+            //Getting longitude and latitude
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+
+            //getFrLoc();
+            //longitude = longi;
+            //latitude = lati;
+
+            //moving the map to location
+            moveMap();
+        }
+    }
+
+    //Function to move the map
+    private void moveMap() {
+        //String to display current latitude and longitude
+        String msg = latitude + ", "+longitude;
+
+        //Creating a LatLng Object to store Coordinates
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        //Adding marker to map
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng) //setting position
+                .title("Your Friend's Location")); //Adding a title
+
+        //Moving the camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        //Animating the camera
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        //Displaying current coordinates in toast
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+
+
+
+             @Override
+             public boolean onOptionsItemSelected(MenuItem item) {
+                 if (item.getItemId() == android.R.id.home) {
+                     MapsActivity.this.finish();
+                     return true;
+                 }
+                 return super.onOptionsItemSelected(item);
+             }
+
 }
